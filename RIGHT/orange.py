ca71e9e78287abb35a45e06a846945bf05875aab
gaellebega <gaellebega@gmail.com>
2026-02-25 12:32:33 +0200

    apples and orange problem done

--- RIGHT/orange.py
+++ RIGHT/orange.py
@@ -1,6 +1,27 @@
- class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        for i in nums:
-            if nums.count(i)>1:
-                return True
-        return False      
+# 7,11
+s,t=list(map(int,input().split()))
+# 5,15
+appleposi,orangepos=list(map(int,input().split()))
+# 3,2
+applenum,orangenum=list(map(int,input().split()))
+# [-2,2,1]
+appledis=list(map(int,input().split()))
+# [5,-6]
+orangedis=list(map(int,input().split()))
+# check which fruits lands 
+totalapples=0
+totaloranges=0
+# len cz am gonna need the index
+for i in range(len(appledis)):
+  location1=appleposi+appledis[i]
+  if s<=location1<=t:
+    totalapples+=1
+for i in range(len(orangedis)):
+  location2=orangepos+orangedis[i]
+  if s<=location2<=t:
+    totaloranges+=1
+print(totalapples)
+print(totaloranges)    
+
+
+ 
---
+++ RIGHT/palindromeindex.py
@@ -0,0 +1,9 @@
+n=int(input())
+string=input()
+if string==string[::-1]:
+    print( -1)
+size=len(string)
+# to check the half of the whole string
+for i in range(n//2):
+    s[i]!=s[n-1-i]:
+     if s
