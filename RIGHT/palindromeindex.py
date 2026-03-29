7b334f37532d1f9018db5eca4fd8ac12b3f546c3
gaellebega <gaellebega@gmail.com>
2026-03-29 15:17:30 +0200

    palindrome index

--- RIGHT/palindromeindex.py
+++ RIGHT/palindromeindex.py
@@ -1,9 +1,17 @@
-n=int(input())
-string=input()
-if string==string[::-1]:
-    print( -1)
-size=len(string)
-# to check the half of the whole string
-for i in range(n//2):
-    s[i]!=s[n-1-i]:
-     if s
+def palindrome_index(s):
+  if s==s[::-1]:
+    return -1
+  right=len(s)-1
+  left=0
+  while(left<right):
+    if s[right]==s[left]:
+      right-=1
+      left+=1
+    else:
+      if s[left+1:right+1]==s[left+1:right+1][::-1]:
+        return left
+      elif s[left:right]==s[left:right][::1]:
+        return right
+    return -1
+  return -1    
+
